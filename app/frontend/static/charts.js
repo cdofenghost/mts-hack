50980dc6eba1dd6b645bfabda6a3713418f14877
dofenghost <dofenspot@gmail.com>
2025-05-21 18:14:47 +0700

    Frontend shenanigans (almost done)

--- app/frontend/static/charts.js
+++ app/frontend/static/charts.js
@@ -1,92 +1,155 @@
 const ctx = document.getElementById('graphChart').getContext('2d');
 
 const gradient = ctx.createLinearGradient(0, 0, 0, 400);
-gradient.addColorStop(0, 'rgba(255, 0, 50, 0.5)');
+gradient.addColorStop(0, 'rgba(255, 0, 51, 0.3)');
 gradient.addColorStop(1, 'rgba(241, 126, 145, 0.125)');
 
-let chart = new Chart(ctx, {
-    type: "line",
-    data: {
-        labels: ['+0', '+1', '+2', '+3', '+4', '+5', '+6'],
-        datasets: [{
-            label: 'One',
-            data: [1000.00, 1020.00, 980.00, 1050.00, 1000.00, 1070.00, 1040.00],
-            fill: true,
-            backgroundColor: gradient,
-            borderColor: "#FF0032",
-            tension: 0.4,
-        }],
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-            legend: {
-                display: false,
-                labels: {
-                    font: {
-                        family: "MTSTextRegular",
-                    }
-                }
-            },
-            tooltip: {
-                callbacks: {
-                    label: function (tooltipItem) {
-                        return "$" + tooltipItem.yLabel.toFixed(2);
-                    }
-                },
-                bodyFont: {
-                    family: "MTSTextRegular",
-                }
-            },
+let labels = [];
+let humidities = [];
+let temperatures = [];
+let pressures = [];
+let windSpeeds = [];
+let windDirections = [];
+let datasets = null;
+
+let chart = null;
+
+let predictions = null;
+
+document.addEventListener('DOMContentLoaded', async function() {
+  try {
+    const response = await fetch('http://127.0.0.1:8000/predict', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
         },
-        scales: {
-            x: {
-                grid: {
-                    drawOnChartArea: false,
-                    drawBorder: true,
+        body: JSON.stringify({
+            station: "string",
+            time: "string",
+        }),
+    });
+    predictions = await response.json();
+    let len = await Object.keys(predictions.predictions).length;
+    console.log(predictions.predictions);
+
+    let perHour = (Number)(len / 24);
+    for (let i = 0; i < len; i++) {
+        var hour = Math.floor(i / perHour);
+        var minute = (i % (perHour)) * 2;
+        var time = `+${hour}:${minute}0`;
+
+        console.log(i, i % perHour, Math.floor(i / perHour));
+        labels.push(`${time}`);
+
+        // if (i % perHour === 0)
+        //     labels.push(`${time}`);
+        // else
+        //     labels.push('');
+
+        humidities.push(predictions.predictions[i].humidity[0]);
+        pressures.push(predictions.predictions[i].pressure[0]);
+        temperatures.push(predictions.predictions[i].temperature[0]);
+        windDirections.push(predictions.predictions[i].wind_direction[0]);
+        windSpeeds.push(predictions.predictions[i].wind_speed[0]);
+
+    }
+    console.log(labels);
+    console.log(humidities);
+    console.log(pressures);
+    console.log(temperatures);
+    console.log(windSpeeds);
+    console.log(windDirections);
+
+
+    chart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'One',
+                data: humidities,
+                fill: true,
+                backgroundColor: gradient,
+                borderColor: "#FF0032",
+                tension: 0.4,
+            }],
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+                legend: {
+                    display: false,
+                    labels: {
+                        font: {
+                            family: "MTSTextRegular",
+                        }
+                    }
                 },
-                ticks: {
-                    font: {
-                        family: "MTSTextRegular",
+                tooltip: {
+                    enabled: true,
+                    callbacks: {
+                        label: function (tooltipItem) {
+                            return tooltipItem.raw.toFixed(6);
+                        }
                     },
-                    color: "#FF0032",
-                }
+                    bodyFont: {
+                        family: "MTSTextRegular",
+                    }
+                },
             },
-            y: {
-                display: false,
-                grid: {
-                    drawOnChartArea: false,
-                    drawBorder: true,
-                },
-                ticks: {
-                    font: {
-                        family: "MTSTextRegular",
+            scales: {
+                x: {
+                    grid: {
+                        drawOnChartArea: false,
+                        drawBorder: true,
                     },
+                    ticks: {
+                        font: {
+                            family: "MTSTextRegular",
+                            size: 18,
+                        },
+                        color: "#000",
+                    }
+                },
+                y: {
+                    grid: {
+                        drawOnChartArea: false,
+                        drawBorder: true,
+                    },
+                    ticks: {
+                        font: {
+                            family: "MTSTextRegular",
+                            size: 18,
+                        },
+                    }
                 }
             }
         }
-    }
-});
+    });
 
-const datasets = {
-    One: {
-        label: "One",
-        data: [1000.00, 1020.00, 980.00, 1050.00, 1000.00, 1070.00, 1040.00],
-    },
-    Two: {
-        label: "Two",
-        data: [950.00, 920.00, 930.00, 900.00, 980.00, 970.00, 950.00],
-    },
-    Three: {
-        label: "Three",
-        data: [1100.00, 1120.00, 1180.00, 1150.00, 1100.00, 1170.00, 1140.00],
-    },
-    Four: {
-        label: "Four",
-        data: [1200.00, 1240.00, 1220.00, 1270.00, 1290.00, 1280.00, 1290.00],
-    },
-};
+    datasets = {
+        Humidity: {
+            label: "Влажность",
+            data: humidities,
+        },
+        Pressure: {
+            label: "Давление",
+            data: pressures,
+        },
+        Temperature: {
+            label: "Температура",
+            data: temperatures,
+        },
+        WindDirection: {
+            label: "Направление ветра",
+            data: windDirections,
+        },
+        
+    };
+  }
+  catch { ; }
+});
 
 function updateChart(company) {
     const { label, data } = datasets[company];
