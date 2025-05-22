318d09e158cb6f6695c4394adc59f31a90a499b3
Oleg <oleg_shaforostov@mail.ru>
2025-05-22 12:44:29 +0700

    main add

---
+++ app/backend/main.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import uvicorn
+from fastapi import FastAPI
+from starlette.staticfiles import StaticFiles
+
+from app.backend.routes import router
+
+app = FastAPI()
+BASE_DIR = Path(__file__).resolve().parent.parent
+FRONTEND_DIR = BASE_DIR / "frontend"
+app.include_router(router)
+
+app.mount("/static", StaticFiles(directory=FRONTEND_DIR / "static"), name="static")
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app="app.backend.main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True
+    )
